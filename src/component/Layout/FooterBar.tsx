1200253ca5262a657afd383acb71f161bf1d7f3d
Dave <davidglogowski2003@gmail.com>
2025-06-17 22:21:37 +0200

    feat(footer): add new icons and routing to footer

--- src/component/Layout/FooterBar.tsx
+++ src/component/Layout/FooterBar.tsx
@@ -3,10 +3,11 @@ import { FaHouse } from "react-icons/fa6";
 import { FaSearch, FaRegBell } from "react-icons/fa";
 import { FiMail } from "react-icons/fi";
 import ComposePostMobileButton from "../ButtonComponent/ComposePostMobileButton";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { useCurrentUser } from "../../hooks/queries/CurrentUserProvider";
 import { useNotifications } from "../../hooks/queries/useNotifications";
 import { useMemo } from "react";
+import { HeroIcon } from "../../types/HeroIcon";
 
 
 
@@ -15,7 +16,8 @@ function FooterBar () {
     const navigate = useNavigate();
     const {currentUser} = useCurrentUser();
     const { data: notifications = [] } = useNotifications(currentUser?.id);
-    
+    const location = useLocation();
+
     const areUnread = useMemo(() => {
     return notifications.some(notification => !notification.seen);
     }, [notifications]);
@@ -26,24 +28,25 @@ function FooterBar () {
             <div className="h-14 w-full bg-(--background-main) absolute bottom-0 text-2xl text-white border-t border-t-(--twitter-border) flex items-center justify-around">
 
                 <div className="w-full h-full flex items-center justify-center">
-                    
-                    <FaHouse className="hover:cursor-pointer" onClick={() => navigate("/")}/>
+                    <div onClick={() => currentUser ? navigate("/") : null}>
+                        <HeroIcon iconName="HomeIcon" className="w-7 h-7" solid={location.pathname === `/`}/>
+                    </div>
                 </div>
                 <div className="w-full h-full flex items-center justify-center">
-                    <FaSearch />
+                    <div>
+                        <HeroIcon iconName="MagnifyingGlassIcon" className="w-7 h-7" solid={location.pathname === `/explore`}/>
+                    </div>
                 </div>
                 <div className="w-full h-full flex relative items-center justify-center">
-                    <FaRegBell onClick={() => {
-                        if (currentUser) {
-                            navigate("/notifications")
-                        }
-                    }} />
+                    <div onClick={() => currentUser ? navigate("/notifications") : null}>
+                    <HeroIcon iconName="BellIcon" className="w-7 h-7" solid={location.pathname === `/notifications`}/>
+                    </div>
                     {currentUser && areUnread && (
                         <div className="w-4 rounded-full h-4 absolute z-40 bg-(--color-main) top-2 right-9">  </div>
                     )}
                 </div>
                 <div className="w-full h-full flex items-center justify-center">
-                    <FiMail />
+                    <HeroIcon iconName="EnvelopeIcon" className="w-7 h-7" solid={location.pathname === `/messages`}/>
                 </div>
 
                 {currentUser && (
