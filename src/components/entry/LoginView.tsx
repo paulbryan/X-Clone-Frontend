abd2ded78c535b335d9997f50b8fb636a4b9e096
Dave <davidglogowski2003@gmail.com>
2025-07-06 23:58:30 +0200

    chore(login): remove unused admin login

--- src/components/entry/LoginView.tsx
+++ src/components/entry/LoginView.tsx
@@ -1,9 +1,9 @@
-import { useAuth } from '../../context/Auth/AuthProvider.tsx';
+// import { useAuth } from '../../context/Auth/AuthProvider.tsx';
 import type { ModalType } from '../../lib/types/ModalType.ts';
 import { FaXTwitter } from 'react-icons/fa6';
 import { GoogleAuthButton } from '../ui/GoogleAuthButton.tsx';
 import { HorizontalStripedText } from '../ui/HorizontalStripedText.tsx';
-import { API_URL } from '../../constants/env.ts';
+// import { API_URL } from '../../constants/env.ts';
 import { UseTempAccountButton } from './UseTempAccountButton.tsx';
 
 
@@ -14,22 +14,22 @@ type LoginViewProps = {
 
 function LoginView ({ setToggle }: LoginViewProps) {
   
-    const { setAuthId } = useAuth();
+    // const { setAuthId } = useAuth();
   
-    function adminLoginQuick(id: number) {
-      fetch(`${API_URL}/api/users/getAdminUser?id=${id}`)
-        .then(res => {
-          if (!res.ok) throw new Error("Admin login failed");
-          return res.json();
-        })
-        .then(user => {
-          setAuthId(user.id);
-          setToggle(null);
-        })
-        .catch(err => {
-          console.error("Admin login error:", err);
-        });
-    }
+    // function adminLoginQuick(id: number) {
+    //   fetch(`${API_URL}/api/users/getAdminUser?id=${id}`)
+    //     .then(res => {
+    //       if (!res.ok) throw new Error("Admin login failed");
+    //       return res.json();
+    //     })
+    //     .then(user => {
+    //       setAuthId(user.id);
+    //       setToggle(null);
+    //     })
+    //     .catch(err => {
+    //       console.error("Admin login error:", err);
+    //     });
+    // }
     return (
 
       <div className="w-full h-full flex flex-col border text-twitterText rounded-4xl p-8 items-center gap-6 bg-(--background-main)">
@@ -48,10 +48,6 @@ function LoginView ({ setToggle }: LoginViewProps) {
 
         <p className="text-md w-full font text-twitterTextAlt">
           Don't have an account? <span className='text-(--color-main)'>Sign up</span>
-        </p>
-
-        <p className="text-md w-full font text-twitterTextAlt">
-          Are you the admin? <span onClick={() => adminLoginQuick(13)} className='text-(--color-main)'>Sign in here</span>
         </p>
 
     </div>
