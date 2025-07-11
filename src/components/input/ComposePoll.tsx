549cd225e4d29318decc9d543826f6aa09afd23c
Dave <davidglogowski2003@gmail.com>
2025-07-11 16:38:52 +0200

    style(poll): style poll compose on compose tweet

--- src/components/input/ComposePoll.tsx
+++ src/components/input/ComposePoll.tsx
@@ -1,16 +1,39 @@
 export function ComposePoll () {
 
     return (
-        <div className="w-full h-full flex flex-col gap-2 p-2">
+        <div className="w-full h-full flex flex-col gap-2 px-4 pt-4 border border-twitterBorder rounded-2xl mb-4">
 
-        <input
-            className="w-full border border-twitterBorder focus:outline-none focus:ring-0 rounded-xl px-2 h-16"
-            placeholder="Choice 1"
-        />
-        <input
-            className="w-full border border-twitterBorder focus:outline-none focus:ring-0 rounded-xl px-2 h-16"
-            placeholder="Choice 2"
-        />
+        <div className="flex flex-col w-full gap-4 mb-4">
+        <div className="flex w-full flex-col gap-2">
+          <p className="pl-1 text-twitterTextAlt font-bold">
+            Choice 1
+          </p>
+          <input
+            className="w-full border border-twitterBorder focus:outline-none focus:ring-0 rounded-xl px-2 h-12"
+          />
+        </div>
+        <div className="flex w-full flex-col gap-2">
+          <p className="pl-1 text-twitterTextAlt font-bold">
+            Choice 2
+          </p>
+          <input
+            className="w-full border border-twitterBorder focus:outline-none focus:ring-0 rounded-xl px-2 h-12"
+          />
+        </div>
+        </div>
+
+        <hr className="text-twitterBorder"/>
+
+        <div
+        className={`hover:cursor-pointer text-red-500 flex items-center gap-2 justify-center h-10 rounded-full`}
+      >
+        <p className="">Remove Poll</p>
+      </div>
+
+        <div>
+
+        </div>
+
 
         </div>
     )
