17cd7d0f218f5a4f6edacc7166ad5ded534eda73
Dave <davidglogowski2003@gmail.com>
2025-07-15 16:29:08 +0200

    style: remove box around poll choices and add color main hover bg

--- src/components/tweet/Poll.tsx
+++ src/components/tweet/Poll.tsx
@@ -11,7 +11,7 @@ type PollProps = {
 export function Poll ({pollId}: PollProps) {
 
     const {data: pollChoices} = usePollChoices(pollId);
-    
+
 
 
     useEffect(() => {
@@ -24,12 +24,12 @@ export function Poll ({pollId}: PollProps) {
     return (
         <div
         onClick={(e) => e.stopPropagation()}
-        className={`w-full z-40 bg-(--background-main) hover:cursor-default hover:bg-(--background-main) h-full flex flex-col gap-2 px-4 pt-4 border border-twitterBorder rounded-2xl`}>
+        >
 
-        <div className="flex flex-col w-full gap-4 mb-4">
+        <div className="flex flex-col w-full hover:cursor-default gap-2 mb-4">
           { pollChoices && pollChoices.map((choice, index) => (
               <div
-                className="w-full flex items-center border border-twitterBorder focus:outline-none focus:ring-0 rounded-xl px-4 h-12"
+                className="w-full flex items-center hover:cursor-pointer hover:bg-(--color-main)/10 border-2 border-(--color-main) focus:outline-none focus:ring-0 rounded-4xl px-4 h-12"
               >
                 <p>{choice.choice}</p>
               </div>  
