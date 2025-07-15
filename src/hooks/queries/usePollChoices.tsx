d51415c34cd523dbed84c5cc6ff984a2d9099c70
Dave <davidglogowski2003@gmail.com>
2025-07-15 15:02:56 +0200

    fix(poll choices): fix api path and change from string[] to PollChoice type

--- src/hooks/queries/usePollChoices.tsx
+++ src/hooks/queries/usePollChoices.tsx
@@ -1,10 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
-import { notificationBatcher } from "../batcher/notificationBatcher.tsx";
+import type { PollChoice } from "../../types/PollChoice";
+import { API_URL } from "../../constants/env";
+
 export const usePollChoices = (pollId: number) =>
-    useQuery<string[]>({
+    useQuery<PollChoice[]>({
       queryKey: ["poll_choices", pollId],
       queryFn: async () => {
-        const response = await fetch(`/api/polls/${pollId}/choices`);
+        const response = await fetch(`${API_URL}/api/polls/${pollId}/choices`);
         if (!response.ok) throw new Error("Failed to fetch unseen notifications");
         return response.json(); // should be an array of PollChoice
       },
