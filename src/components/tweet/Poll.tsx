f965f69b28fc430900b2d3a40f20f0d2736fead8
Dave <davidglogowski2003@gmail.com>
2025-07-15 18:00:51 +0200

    add voted conditional rendering for poll and hook to submit vote

--- src/components/tweet/Poll.tsx
+++ src/components/tweet/Poll.tsx
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 import { usePollChoices } from "../../hooks/queries/usePollChoices";
 import { HeroIcon } from "../ui/icons/HeroIcon";
+import { useHasVoted } from "../../hooks/queries/useHasVoted";
+import { useVoteOnPoll } from "../../hooks/mutations/useVoteOnPoll";
 
 type PollProps = {
 
@@ -11,8 +13,14 @@ type PollProps = {
 export function Poll ({pollId}: PollProps) {
 
     const {data: pollChoices} = usePollChoices(pollId);
+    const {data: hasVoted} = useHasVoted(pollId);
+    const hasAlreadyVoted = hasVoted && hasVoted != -1;
+    const voteMutation = useVoteOnPoll(pollId); // Don't pass choiceId yet
 
-
+    const handleVote = (choiceId: number) => {
+      if (hasAlreadyVoted) return;
+      voteMutation.mutate({ choiceId });
+    };
 
     useEffect(() => {
         console.log("Poll id is: " + [pollId])
@@ -21,21 +29,54 @@ export function Poll ({pollId}: PollProps) {
         }
     }, [pollChoices])
 
+
+
     return (
         <div
         onClick={(e) => e.stopPropagation()}
         >
 
-        <div className="flex flex-col w-full hover:cursor-default gap-2 mb-4">
-          { pollChoices && pollChoices.map((choice, index) => (
-              <div
-                className="w-full flex items-center hover:cursor-pointer hover:bg-(--color-main)/10 border-2 border-(--color-main) focus:outline-none focus:ring-0 rounded-4xl px-4 h-12"
-              >
-                <p>{choice.choice}</p>
-              </div>  
+<div className="flex flex-col w-full gap-2 mb-4">
+  {pollChoices &&
+    (() => {
+      const totalVotes = pollChoices.reduce((sum, c) => sum + c.voteCount, 0);
 
-          ))}
-        </div>
+      return pollChoices.map((choice) => {
+        const percent =
+          totalVotes > 0 ? Math.round((choice.voteCount / totalVotes) * 100) : 0;
+
+        return hasAlreadyVoted ? (
+          <div
+            key={choice.id}
+            className="relative w-full text-sm rounded-xl overflow-hidden  h-12 px-4 flex items-center"
+          >
+            <div
+              className="absolute top-0 left-0 h-full bg-twitterText opacity-20"
+              style={{ width: `${percent}%` }}
+            />
+
+            <div className="flex text-sm text-twitterText w-full justify-between items-center relative z-10">
+              <div className="flex gap-2 items-center">
+                <p>{choice.choice}</p>
+                {hasVoted === choice.id && (
+                  <HeroIcon iconName="CheckCircleIcon" className="w-4 h-4" />
+                )}
+              </div>
+              <p className="">{percent}%</p>
+            </div>
+          </div>
+        ) : (
+          <div
+            key={choice.id}
+            onClick={() => handleVote(choice.id)}
+            className="w-full border-2 border-twitterBorder text-sm rounded-4xl h-12 px-4 flex items-center hover:bg-twitterBlue/10 cursor-pointer"
+          >
+            <p>{choice.choice}</p>
+          </div>
+        );
+      });
+    })()}
+</div>
 
         <div>
 
