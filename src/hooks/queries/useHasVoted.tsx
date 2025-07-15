a249adc4a96d809fbeca340e44144470b55ad30f
Dave <davidglogowski2003@gmail.com>
2025-07-15 18:34:32 +0200

    fix: updated useHasVoted hook to use auth and rename from usChoiceVote to useHasVoted

--- src/hooks/queries/useHasVoted.tsx
+++ src/hooks/queries/useHasVoted.tsx
@@ -1,14 +1,22 @@
 import { useQuery } from "@tanstack/react-query";
 import { API_URL } from "../../constants/env";
 
-export const useChoiceVote = (pollId: number) =>
-    useQuery<number>({
-      queryKey: ["poll_votes", pollId],
-      queryFn: async () => {
-        const response = await fetch(`${API_URL}/api/polls/${pollId}/getPollVote`);
-        if (!response.ok) throw new Error("Failed to fetch poll vote");
-        return response.json();
-      },
-      enabled: typeof pollId === "number" && pollId > 0,
-      staleTime: 1000 * 60 * 5,
-    });
+export const useHasVoted = (pollId: number) =>
+  useQuery<number>({
+    queryKey: ["poll_votes", pollId],
+    queryFn: async () => {
+      const token = localStorage.getItem("jwt");
+
+      const response = await fetch(`${API_URL}/api/polls/${pollId}/getPollVote`, {
+        headers: {
+          "Content-Type": "application/json",
+          ...(token ? { Authorization: `Bearer ${token}` } : {}),
+        },
+      });
+
+      if (!response.ok) throw new Error("Failed to fetch poll vote");
+      return response.json();
+    },
+    enabled: typeof pollId === "number" && pollId > 0,
+    staleTime: 1000 * 60 * 5,
+  });
