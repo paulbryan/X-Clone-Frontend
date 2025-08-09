0aef3c43e20c533ced0ae6c37a71670c5fd093d2
Dave <davidglogowski2003@gmail.com>
2025-08-09 22:15:14 +0200

    Delete old unused auth context

--- src/main.tsx
+++ src/main.tsx
@@ -5,17 +5,14 @@ import App from './App.tsx'
 import TimeAgo from 'javascript-time-ago'
 import en from 'javascript-time-ago/locale/en'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { AuthProvider } from './context/Auth/AuthProvider.tsx'
 
 TimeAgo.addDefaultLocale(en)
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AuthProvider>
     <QueryClientProvider client={queryClient}>
     <App />
     </QueryClientProvider>
-    </AuthProvider>
   </StrictMode>,
 )
